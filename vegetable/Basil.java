94fc283a5b6bf613e83640a47e8597df899776a0
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-07-18 10:36:53 +0300

    Create Basil.java

---
+++ vegetable/Basil.java
@@ -0,0 +1,11 @@
+package vegetable;
+/**
+ *
+ * @author Admin
+ */
+public class Basil extends Spicy{
+    
+    public Basil(int quantity){
+        super("Базилик", "Зеленый", 22.0, quantity);
+    }
+}
