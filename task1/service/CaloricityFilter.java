2d7ec5085ab17ec53306164f7444b00efc0d5764
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-07-22 12:01:05 +0300

    Update and rename Filter.java to CaloricityFilter.java

--- task1/service/CaloricityFilter.java
+++ task1/service/CaloricityFilter.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entity.*;
+import service.logger.DebugLogger;
 
 /**
  *
  * @author Admin
  */
-public class Filter {
+public class CaloricityFilter {
+    
+    private static DebugLogger debugLog = new DebugLogger(DebugLogger.class);
     
     public static List<Vegetable> twoBoundFilter(Salad salad, double min, double max){
+        debugLog.getLogger().debug("Two bounds filtration\n");
         List<Vegetable> tmp = new ArrayList<>();
         for(Vegetable vegi : salad.getIngredients()){
             if(vegi.getCaloricity() > min && vegi.getCaloricity() < max){
@@ -20,5 +24,4 @@ public class Filter {
         }
         return tmp;
     }
-    
 }
