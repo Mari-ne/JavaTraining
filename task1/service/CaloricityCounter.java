f3297fd99af8e90380f8c95da9d6fb2eab186bc7
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-07-22 12:00:14 +0300

    Update CaloricityCounter.java

--- task1/service/CaloricityCounter.java
+++ task1/service/CaloricityCounter.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import entity.Vegetable;
 import entity.Salad;
-//import entity.FlavoredSalad;
+import service.logger.DebugLogger;
 
 /**
  *
@@ -12,7 +12,10 @@ import entity.Salad;
  */
 public class CaloricityCounter {
     
+    private static DebugLogger debugLog = new DebugLogger(DebugLogger.class);
+    
     public static double countTotalCaloricity(Salad salad){
+        debugLog.getLogger().debug("Count total caloricity of salad\n");
         double tmpCaloricity = 0;
         List<Vegetable> ingredients = salad.getIngredients();
         for(Vegetable vegetable : ingredients){
