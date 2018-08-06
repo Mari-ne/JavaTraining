d3142bd0b44355a38ebfa9376d5c866f7f10de50
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-08-06 09:22:54 +0300

    Update MainLogic.java

--- parser/service/MainLogic.java
+++ parser/service/MainLogic.java
@@ -1,9 +1,7 @@
 package parser.service;
 
 import java.util.List;
-
 import org.apache.log4j.Logger;
-
 import java.util.ArrayList;
 
 import parser.entity.*;
@@ -18,30 +16,26 @@ public class MainLogic {
 	private static final Logger LOGGER = Logger.getRootLogger();
 
 	public static List<String> wordSearch (CompositeTextFragment text, int size){
+		LOGGER.debug("Search of words with length " + size + " in text.");
 		List<String> sizeWords = new ArrayList<>();
-		//List<TextFragment> fragments = text.getChildren();
-		for(TextFragment fragment : text.getChildren()) {
-			if(fragment instanceof Listing || fragment instanceof PunctuationMark) {
-				//if fragment is listing it can't have question sentences
+		for(TextFragment paragraph : text.getChildren()) {
+			if(paragraph instanceof Leaf) {
+				//listing & end of paragraph
 				continue;
 			}
-			CompositeTextFragment paragraph = (CompositeTextFragment)fragment;
-			List<TextFragment> sentences = paragraph.getChildren();
-			for(int i = 1; i < sentences.size(); i += 2) {
-				//every second element in paragraph - mark of end of sentence
-				if(((PunctuationMark)sentences.get(i)).isQestion()) {
-					CompositeTextFragment words = (CompositeTextFragment)sentences.get(i -1);
-					for(TextFragment word : words.getChildren()) {
-						Word w = (Word)word;
-						if(w.size() == size && !sizeWords.contains(w.getWord())) {
-							LOGGER.debug("Add new suit word - " + w.getWord());
-							sizeWords.add(w.getWord());
+			List<TextFragment> parag = ((CompositeTextFragment)paragraph).getChildren();
+			for(int i = 1; i < parag.size(); i +=2) {
+				Leaf endMark = (Leaf)parag.get(i);
+				if(endMark.getData().equals("?")) {
+					for(TextFragment word : ((CompositeTextFragment)parag.get(i -1)).getChildren()) {
+						Leaf w = (Leaf)word;
+						if(w.getData().trim().length() == size && !sizeWords.contains(w.getData())) {
+							sizeWords.add(w.getData());
 						}
 					}
 				}
 			}
 		}
-		
 		return sizeWords;
 	}
 }
