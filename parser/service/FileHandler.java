6223aed035a9afee07786dd4b75ad1be36327c2b
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-08-06 09:22:33 +0300

    Update FileHandler.java

--- parser/service/FileHandler.java
+++ parser/service/FileHandler.java
@@ -1,10 +1,12 @@
 package parser.service;
 
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,12 +20,13 @@ import org.apache.log4j.Logger;
  *
  */
 public class FileHandler {
-	private File file;
+	private String filePath;
 	
 	private static final Logger LOGGER = Logger.getRootLogger();
 		
 	public FileHandler(String filePath) throws IOException, FileNotFoundException{
-		file = new File(filePath);
+		this.filePath = filePath;
+		File file = new File(filePath);
 		if(file.length() == 0) {
 			throw new IOException("Empty file.");
 		}
@@ -38,17 +41,22 @@ public class FileHandler {
   	}
 	
 	public String read() throws IOException {
-		LOGGER.debug("Reading data from " + file.getPath().toString());
-		byte[] fileData = Files.readAllBytes(file.toPath());
-		return new String(fileData, Charset.defaultCharset());
+		LOGGER.debug("Reading data from " + filePath);
+		FileReader reader = new FileReader(filePath);
+		StringBuilder string = new StringBuilder("");
+		int data;	
+		while((data = reader.read()) != -1) {
+			string.append((char)data);
+		}
+		reader.close();
+		return string.toString();
 	}
 		
 	public void write(String s) throws IOException {
-		Files.write(file.toPath(), s.getBytes(), StandardOpenOption.APPEND);
-	}
-	
-	public void write(char c) throws IOException {
-		byte[] data = {(byte)c};
-		Files.write(file.toPath(), data, StandardOpenOption.APPEND);
+		LOGGER.debug("Writing data to " + filePath);
+		FileWriter writer = new FileWriter(filePath, true);
+		BufferedWriter bufWriter = new BufferedWriter(writer);
+		bufWriter.write(s);
+		bufWriter.close();
 	}
 }
