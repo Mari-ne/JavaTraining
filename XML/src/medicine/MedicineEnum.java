7130a5d6c4ad8d2d6e2c2235490d8ad68a94e10f
Mari-ne <40910834+Mari-ne@users.noreply.github.com>
2018-08-22 08:54:03 +0300

    Add files via upload

---
+++ XML/src/medicine/MedicineEnum.java
@@ -0,0 +1,42 @@
+package medicine;
+
+public enum MedicineEnum {
+	MEDICINES("medicines"),
+	NAME("name"),
+	PHARM("pharm"),
+	GROUP("group"),
+	VALID("valid"),
+	CERTIFICATED("certificated"),
+	ANALOGS("analogs"),
+	PRICE("price"),
+	WRAPPED("wrapped"),
+	FORM("form"),
+	AMOUNT("amount"),
+	FREQ("frequency"),
+	PERIOD("period"),
+	NUMBER("number"),
+	ISSUED("issued"),
+	UNTIL("until"),
+	REG("register"),
+	DOSAGE("dosage"),
+	CERCIFICATE("certificate");
+	
+	private String value;
+	
+	private MedicineEnum(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public static MedicineEnum getEnum(String value) {
+		for(MedicineEnum elem : MedicineEnum.values()) {
+			if(elem.getValue().equals(value)) {
+				return elem;
+			}
+		}
+		return null;
+	}
+}
